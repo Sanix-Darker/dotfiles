09bf2c49e3bcf855eac3ccf8b0db4a51a0a7b9a1
sanix-darker <s4nixd@gmail.com>
2022-12-03 16:30:30 +0100

    feat: updates for sam. 03 déc. 2022 16:30:30 CET

--- init.lua
+++ init.lua
@@ -85,34 +85,88 @@ require('impatient')
 -- set up an appropriate complation like lsp-nvim
 
 
+-- LSP nvim-cmp (much faster than coc-nvim)
+
+vim.opt.completeopt = {'menu', 'menuone', 'noselect'}
 -- Set up nvim-cmp.
 local cmp = require'cmp'
+local luasnip = require'luasnip'
+-- Selected option
+local select_opts = {behavior = cmp.SelectBehavior.Select}
 
 cmp.setup({
     snippet = {
         expand = function(args)
-            require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+            luasnip.lsp_expand(args.body)
         end,
     },
     window = {
-        -- completion = cmp.config.window.bordered(),
-        -- documentation = cmp.config.window.bordered(),
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
     },
     mapping = cmp.mapping.preset.insert({
-        ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+        ['<Up>'] = cmp.mapping.select_prev_item(select_opts),
+        ['<Down>'] = cmp.mapping.select_next_item(select_opts),
+        ['<C-u>'] = cmp.mapping.scroll_docs(-4),
         ['<C-f>'] = cmp.mapping.scroll_docs(4),
         ['<C-Space>'] = cmp.mapping.complete(),
         ['<C-e>'] = cmp.mapping.abort(),
         ['<CR>'] = cmp.mapping.confirm({ select = true }),
-        -- Accept currently selected item. Set `select` to `false`
-        -- to only confirm explicitly selected items.
+        ['<C-d>'] = cmp.mapping(function(fallback)
+          if luasnip.jumpable(1) then
+            luasnip.jump(1)
+          else
+            fallback()
+          end
+        end, {'i', 's'}),
+        ['<C-s>'] = cmp.mapping(function(fallback)
+          if luasnip.jumpable(-1) then
+            luasnip.jump(-1)
+          else
+            fallback()
+          end
+        end, {'i', 's'}),
+        ['<Tab>'] = cmp.mapping(function(fallback)
+          local col = vim.fn.col('.') - 1
+
+          if cmp.visible() then
+            cmp.select_next_item(select_opts)
+          elseif col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
+            fallback()
+          else
+            cmp.complete()
+          end
+        end, {'i', 's'}),
+        ['<S-Tab>'] = cmp.mapping(function(fallback)
+          if cmp.visible() then
+            cmp.select_prev_item(select_opts)
+          else
+            fallback()
+          end
+        end, {'i', 's'}),
     }),
     sources = cmp.config.sources({
+        { name = 'path' },
         { name = 'nvim_lsp' },
+        { name = 'buffer' },
         { name = 'luasnip' }, -- For luasnip users.
     }, {
         { name = 'buffer' },
-    })
+    }),
+    formatting = {
+      fields = {'menu', 'abbr', 'kind'},
+      format = function(entry, item)
+        local menu_icon = {
+          nvim_lsp = 'λ',
+          luasnip = '⋗',
+          buffer = 'Ω',
+          path = '⚎',
+        }
+
+        item.menu = menu_icon[entry.source.name]
+        return item
+      end,
+    },
 })
 
 -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
@@ -133,57 +187,120 @@ cmp.setup.cmdline(':', {
     })
 })
 
--- -- Set up lspconfig.
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
--- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
-require'lspconfig'.pyright.setup {
-    capabilities = capabilities
-}
-require'lspconfig'.tsserver.setup{
-    capabilities = capabilities
-}
-require'lspconfig'.eslint.setup{
-    capabilities = capabilities
-}
-require'lspconfig'.jsonls.setup{
-    capabilities = capabilities
-}
-require'lspconfig'.gopls.setup{
-    capabilities = capabilities
-}
-require'lspconfig'.phpactor.setup{
-    capabilities = capabilities
-}
-require'lspconfig'.cssls.setup{
-    capabilities = capabilities
-}
-require'lspconfig'.html.setup{
-    capabilities = capabilities
-}
-require'lspconfig'.bashls.setup{
-    capabilities = capabilities
-}
-require'lspconfig'.clangd.setup{
-    capabilities = capabilities
-}
-require'lspconfig'.cssmodules_ls.setup{
-    capabilities = capabilities
-}
-require'lspconfig'.emmet_ls.setup{
-    capabilities = capabilities
-}
-require'lspconfig'.phpactor.setup{
-    capabilities = capabilities
-}
-require'lspconfig'.ruby_ls.setup{
-    capabilities = capabilities
-}
-require'lspconfig'.vls.setup{
-    capabilities = capabilities
-}
+---
+-- Global Config
+---
 
--- require'lspconfig'.arduino_language_server.setup{}
--- require'lspconfig'.arduino_language_server.setup {
+-- -- Set up lspconfig.
+local lspconfig = require('lspconfig')
+local lsp_defaults = lspconfig.util.default_config
+
+lsp_defaults.capabilities = vim.tbl_deep_extend(
+  'force',
+  lsp_defaults.capabilities,
+  require('cmp_nvim_lsp').default_capabilities()
+)
+
+-- LSP Servers
+-- Replace <YOUR_LSP_SERVER> with each lsp server you've enabled.
+lspconfig.pyright.setup{}
+lspconfig.tsserver.setup{}
+lspconfig.eslint.setup{}
+lspconfig.jsonls.setup{}
+lspconfig.gopls.setup{}
+lspconfig.phpactor.setup{}
+lspconfig.cssls.setup{}
+lspconfig.html.setup{}
+lspconfig.bashls.setup{}
+lspconfig.clangd.setup{}
+lspconfig.cssmodules_ls.setup{}
+lspconfig.emmet_ls.setup{}
+lspconfig.phpactor.setup{}
+lspconfig.ruby_ls.setup{}
+lspconfig.vls.setup{}
+
+-- To load all our snippets
+require('luasnip.loaders.from_vscode').lazy_load()
+
+
+-- keybindings to go to definition/declaration
+vim.api.nvim_create_autocmd('LspAttach', {
+  desc = 'LSP actions',
+  callback = function()
+    local bufmap = function(mode, lhs, rhs)
+      local opts = {buffer = true}
+      vim.keymap.set(mode, lhs, rhs, opts)
+    end
+
+    -- Displays hover information about the symbol under the cursor
+    bufmap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>')
+    -- Jump to the definition
+    bufmap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>')
+    -- Jump to declaration
+    bufmap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>')
+    -- Lists all the implementations for the symbol under the cursor
+    bufmap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>')
+    -- Jumps to the definition of the type symbol
+    bufmap('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>')
+    -- Lists all the references 
+    bufmap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>')
+
+    -- Displays a function's signature information
+    bufmap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<cr>')
+
+    -- Renames all references to the symbol under the cursor
+    bufmap('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>')
+
+    -- Selects a code action available at the current cursor position
+    bufmap('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>')
+    bufmap('x', '<F4>', '<cmd>lua vim.lsp.buf.range_code_action()<cr>')
+
+    -- Show diagnostics in a floating window
+    bufmap('n', 'gl', '<cmd>lua vim.diagnostic.open_float()<cr>')
+
+    -- Move to the previous diagnostic
+    bufmap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<cr>')
+    -- Move to the next diagnostic
+    bufmap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<cr>')
+  end
+})
+
+-- For diagnostics
+local sign = function(opts)
+  vim.fn.sign_define(opts.name, {
+    texthl = opts.name,
+    text = opts.text,
+    numhl = ''
+  })
+end
+
+sign({name = 'DiagnosticSignError', text = '✘'})
+sign({name = 'DiagnosticSignWarn', text = '▲'})
+sign({name = 'DiagnosticSignHint', text = '⚑'})
+sign({name = 'DiagnosticSignInfo', text = ''})
+
+vim.diagnostic.config({
+  virtual_text = true,
+  signs = true,
+  update_in_insert = false,
+  underline = true,
+  severity_sort = false,
+  float = true,
+})
+
+-- Styling with borders and everything
+vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(
+  vim.lsp.handlers.hover,
+  {border = 'rounded'}
+)
+
+vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(
+  vim.lsp.handlers.signature_help,
+  {border = 'rounded'}
+)
+
+-- lspconfig.arduino_language_server.setup{}
+-- lspconfig.arduino_language_server.setup {
 --   cmd = {
 --     "arduino-language-server",
 --     "-cli-config", "/path/to/arduino-cli.yaml",
