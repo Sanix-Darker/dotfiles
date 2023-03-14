68d381ca2df3e1f0660e80c164632478e703967f
sanix-darker <s4nixd@gmail.com>
2023-03-14 20:38:29 +0100

    feat: updates for mar. 14 mars 2023 20:38:29 CET

--- polybar/weather.sh
+++ polybar/weather.sh
@@ -1,5 +1,5 @@
 #/bin/sh
 
-res="$(curl -s -L wttr.in/Cameroon+Douala?format=1)"
+res="$(curl -s -L wttr.in/France+Argenteuil?format=1)"
 # res="$(curl -s -L wttr.in/France+Argenteuil?format=1)"
 echo ${res##* }
