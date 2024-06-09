6d6c168d4db9d2c56026c5d6b1f1e8035473fb71
sanix-darker <s4nixd@gmail.com>
2024-06-09 20:54:33 +0200

    feat: updates for dim. 09 juin 2024 20:54:33 CEST

--- nvim/lua/plugins_config.lua
+++ nvim/lua/plugins_config.lua
@@ -158,10 +158,10 @@ require('gh-actions').setup()
 -- require("trouble").setup{}
 
 -- for aider as pp assistant
-require('aider').setup({
-  auto_manage_context = false,
-  default_bindings = false
-})
+-- require('aider').setup({
+--   auto_manage_context = false,
+--   default_bindings = false
+-- })
 
 -- Set lualine as statusline
 -- See `:help lualine.txt`
